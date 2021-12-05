f3229d8172e0022a4b7cae59a2f50217bfe4fafd
mam3d <immam3d@gmail.com>
2021-12-05 12:58:21 +0330

    Movie model and test

--- movie/models.py
+++ movie/models.py
@@ -4,8 +4,33 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.name
+
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="movie/")
+    category = models.ManyToManyField(Category)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def categories(self):
+        name = ""
+        for category in self.category.all():
+            name += f"{category.name} "
+        return name
 
     def save(self,*args, **kwargs):
         if not self.slug:
--- movie/tests/test_models.py
+++ movie/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from ..models import (
-            Category,
-            )
+        Category,
+        Movie,
+        )
 
 
 class CategoryTest(TestCase):
@@ -14,3 +15,24 @@ class CategoryTest(TestCase):
         self.assertEqual(self.category.name,"ps5 test")
         self.assertEqual(self.category.slug,"ps5-test")
         self.assertEqual(str(self.category),"ps5 test")
+
+class MovieTest(TestCase):
+    def setUp(self):
+        category = Category.objects.create(
+            name = "Horror"
+        )
+        category2 = Category.objects.create(
+            name = "Comedy"
+        )
+        self.movie = Movie(
+            name = "chucky",
+            image = "media/chucky.png",
+            )
+        self.movie.save()
+        self.movie.category.add(category,category2)
+    
+    def test_movie_created(self):
+        self.assertEqual(self.movie.name,"chucky")
+        self.assertEqual(self.movie.slug,"chucky")
+        self.assertEqual(self.movie.categories,"Horror Comedy ")
+        self.assertEqual(str(self.movie),"chucky")
